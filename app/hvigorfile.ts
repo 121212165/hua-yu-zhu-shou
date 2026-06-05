f5dcfd8a940335037913674c5d71e4925bbceb44
刘佳豪 <15720214985@163.com>
2026-06-05 17:02:26 +0800

    fix: 修复根 hvigorfile.ts 使用 appTasks 插件

--- app/hvigorfile.ts
+++ app/hvigorfile.ts
@@ -1,24 +1,6 @@
-// hvigor构建脚本
+import { appTasks } from '@ohos/hvigor-ohos-plugin';
 
 export default {
-  // 构建系统配置
-  system: {
-    // 构建工具版本
-    hvigorVersion: '4.0.2',
-    // 构建插件配置
-    plugins: [
-      {
-        name: 'entry',
-        moduleType: 'entry',
-        srcPath: './entry'
-      }
-    ]
-  },
-  // 模块配置
-  modules: {
-    // entry模块
-    'entry': {
-      buildMode: 'default'
-    }
-  }
+  system: appTasks,
+  plugins: []
 }
